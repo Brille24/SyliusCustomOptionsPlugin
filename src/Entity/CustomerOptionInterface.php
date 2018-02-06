60bc79e2724db23e695a68110da631cd2d00451b
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2018-02-06 18:04:25 +0100

    Changed autoload in composer.json

---
+++ src/Entity/CustomerOptionInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jtolkemit
+ * Date: 06.02.18
+ * Time: 17:45
+ */
+
+namespace Brille24\CustomerOptionsBundle\Entity;
+
+
+class CustomerOptionInterface
+{
+
+}
