1b812cb33540f9903328934ef2bde336676febf7
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2018-02-07 13:30:52 +0100

    Added translatable CustomerOptionValue

---
+++ src/Entity/CustomerOptionValueTranslation.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jtolkemit
+ * Date: 07.02.18
+ * Time: 13:15
+ */
+
+namespace Brille24\CustomerOptionsBundle\Entity;
+
+
+use Sylius\Component\Resource\Model\AbstractTranslation;
+
+class CustomerOptionValueTranslation extends AbstractTranslation implements CustomerOptionValueTranslationInterface
+{
+    /** @var int */
+    protected $id;
+
+    /** @var string */
+    protected $name;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+}
