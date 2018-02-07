baa8cd841f659c39aa22a757d69898d9f432007b
mamazu <maxo.pech@gmail.com>
2018-02-07 13:35:17 +0100

    Added Customer Option Value Price

---
+++ src/Entity/CustomerOptionValuePrice.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+namespace Brille24\CustomerOptionsBundle\Entity;
+
+
+use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
+
+class CustomerOptionValuePrice implements CustomerOptionValuePriceInterface
+{
+    /** @var int|null */
+    private $id;
+
+    /** @var float */
+    private $percent;
+
+    /** @var int */
+    private $amount;
+
+    /** @var string */
+    private $type;
+
+    /** @var CustomerOptionValueInterface|null */
+    private $customerOptionValue;
+
+    /** {@inheritdoc} */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /** {@inheritdoc} */
+    public function getPercent(): float
+    {
+        return $this->percent;
+    }
+
+    /** {@inheritdoc} */
+    public function setPercent(float $percent): void
+    {
+        $this->percent = $percent;
+    }
+
+    /** {@inheritdoc} */
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    /** {@inheritdoc} */
+    public function setAmount(int $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    /** {@inheritdoc} */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /** {@inheritdoc} */
+    public function setType(string $type): void
+    {
+        $allTypes = $this->getAllTypes();
+
+        if (in_array($type, $allTypes)) {
+            $this->type = $type;
+        } else {
+            throw new InvalidTypeException('Invalid type. Possible types are ' . join(', ', $allTypes));
+        }
+    }
+
+    /** {@inheritdoc} */
+    public function getAllTypes(): array
+    {
+        return [
+            CustomerOptionValuePriceInterface::TYPE_FIXED_AMOUNT,
+            CustomerOptionValuePriceInterface::TYPE_PERCENT,
+        ];
+    }
+
+    /** {@inheritdoc} */
+    public function getCustomerOptionValue(): ?CustomerOptionValueInterface
+    {
+        return $this->customerOptionValue;
+    }
+
+    /** {@inheritdoc} */
+    public function setCustomerOptionValue(?CustomerOptionValueInterface $customerOptionValue): void
+    {
+        $this->customerOptionValue = $customerOptionValue;
+    }
+}
---
+++ src/Entity/CustomerOptionValuePriceInterface.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace Brille24\CustomerOptionsBundle\Entity;
+
+interface CustomerOptionValuePriceInterface
+{
+    const TYPE_FIXED_AMOUNT = 'FIXED_AMOUNT';
+    const TYPE_PERCENT = 'PERCENT';
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int;
+
+    /**
+     * @return float
+     */
+    public function getPercent(): float;
+
+    /**
+     * @param float $percent
+     */
+    public function setPercent(float $percent): void;
+
+    /**
+     * @return int
+     */
+    public function getAmount(): int;
+
+    /**
+     * @param int $amount
+     */
+    public function setAmount(int $amount): void;
+
+    /**
+     * @return string
+     */
+    public function getType(): string;
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void;
+
+    /**
+     * Returns all possible type values for the setType function
+     *
+     * @return array
+     */
+    public function getAllTypes(): array;
+
+    /**
+     * @return CustomerOptionValueInterface|null
+     */
+    public function getCustomerOptionValue(): ?CustomerOptionValueInterface;
+
+    /**
+     * @param CustomerOptionValueInterface|null $customerOptionValue
+     */
+    public function setCustomerOptionValue(?CustomerOptionValueInterface $customerOptionValue): void;
+}
