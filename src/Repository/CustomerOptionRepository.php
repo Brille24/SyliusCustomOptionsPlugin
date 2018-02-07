c1e4a6141ff65773fe96e796d8f328412c18b33f
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2018-02-07 11:30:55 +0100

    Added repository for CustomerOption class

---
+++ src/Repository/CustomerOptionRepository.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jtolkemit
+ * Date: 07.02.18
+ * Time: 10:58
+ */
+
+namespace Brille24\CustomerOptionsBundle\Repository;
+
+
+use Brille24\CustomerOptionsBundle\Entity\CustomerOptionInterface;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class CustomerOptionRepository extends EntityRepository implements CustomerOptionRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findOneByCode(string $code): ?CustomerOptionInterface
+    {
+        return $this->createQueryBuilder('o')
+            ->where('o.code = :code')
+            ->setParameter('code', $code)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findByType(string $type): array
+    {
+        return $this->createQueryBuilder('o')
+            ->where('o.type = :type')
+            ->setParameter('type', $type)
+            ->getQuery()
+            ->getArrayResult()
+            ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findByRequired(bool $required): array
+    {
+        return $this->createQueryBuilder('o')
+            ->where('o.required = :required')
+            ->setParameter('required', $required)
+            ->getQuery()
+            ->getArrayResult()
+            ;
+    }
+
+}
---
+++ src/Repository/CustomerOptionRepositoryInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jtolkemit
+ * Date: 07.02.18
+ * Time: 10:55
+ */
+
+namespace Brille24\CustomerOptionsBundle\Repository;
+
+
+use Brille24\CustomerOptionsBundle\Entity\CustomerOptionInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+interface CustomerOptionRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * @param string $code
+     * @return null|CustomerOptionInterface
+     */
+    public function findOneByCode(string $code) : ?CustomerOptionInterface;
+
+    /**
+     * @param string $type
+     * @return array
+     */
+    public function findByType(string $type) : array;
+
+    /**
+     * @param bool $required
+     * @return array
+     */
+    public function findByRequired(bool $required) : array;
+}
