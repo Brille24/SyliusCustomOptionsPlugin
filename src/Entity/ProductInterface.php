2031d7711c3a1202435e0d7f42ca962403642acd
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2018-02-14 10:30:46 +0100

    Extended add to cart form type

--- src/Entity/ProductInterface.php
+++ src/Entity/ProductInterface.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 namespace Brille24\CustomerOptionsPlugin\Entity;
 
+use Brille24\CustomerOptionsPlugin\Entity\CustomerOptions\CustomerOptionGroupInterface;
 use Brille24\CustomerOptionsPlugin\Entity\CustomerOptions\CustomerOptionInterface;
 use Sylius\Component\Core\Model\ProductInterface as BaseProductInterface;
 
 interface ProductInterface extends BaseProductInterface, CustomerOptionInterface
 {
+    public function getCustomerOptionGroup(): ?CustomerOptionGroupInterface;
 
+    public function setCustomerOptionGroup(?CustomerOptionGroupInterface $customerOptionGroup);
 }
