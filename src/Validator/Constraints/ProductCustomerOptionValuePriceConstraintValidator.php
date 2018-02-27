24ddf55c95c4b7883b8fe3e371b2a8c85a7a49c5
mamazu <maxo.pech@gmail.com>
2018-02-27 12:09:43 +0100

    Improved code style

--- src/Validator/Constraints/ProductCustomerOptionValuePriceConstraintValidator.php
+++ src/Validator/Constraints/ProductCustomerOptionValuePriceConstraintValidator.php
@@ -5,12 +5,14 @@
  * Date: 20.02.18
  * Time: 12:34
  */
+declare(strict_types=1);
 
 namespace Brille24\CustomerOptionsPlugin\Validator\Constraints;
 
 
 use Brille24\CustomerOptionsPlugin\Entity\CustomerOptions\CustomerOptionValuePriceInterface;
 use Doctrine\Common\Collections\Collection;
+use InvalidArgumentException;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -19,49 +21,34 @@ class ProductCustomerOptionValuePriceConstraintValidator extends ConstraintValid
     /**
      * {@inheritdoc}
      */
-    public function validate($value, Constraint $constraint): void
+    public function validate($collection, Constraint $constraint): void
     {
-        if(!is_a($value, Collection::class)){
-            throw new \InvalidArgumentException('Value is not a Collection.');
+        if (!is_a($collection, Collection::class)) {
+            throw new InvalidArgumentException('Value is not a Collection.');
         }
 
-        /** @var Collection $value */
-        if($value->isEmpty()){
+        /** @var Collection $collection */
+        if ($collection->isEmpty()) {
             return;
         }
 
-        if(!is_a($value[0], CustomerOptionValuePriceInterface::class)){
-            throw new \InvalidArgumentException('Collection does not contain CustomerOptionValuePrices.');
+        if (!is_a($collection[0], CustomerOptionValuePriceInterface::class)) {
+            throw new InvalidArgumentException('Collection does not contain CustomerOptionValuePrices.');
         }
-
-
-
-//        $existingValues = [];
-//
-//        /** @var CustomerOptionValuePriceInterface $price */
-//        foreach ($value->getValues() as $price){
-//            if($existingValues[$price->getChannel()->getCode()] === $price->getCustomerOptionValue()){
-//                $this->context->addViolation($constraint->message);
-//            }else{
-//                $existingValues[$price->getChannel()->getCode()] = $price->getCustomerOptionValue();
-//            }
-//        }
 
         $existingValues = [];
 
         /** @var CustomerOptionValuePriceInterface $price */
-        foreach($value->getValues() as $price){
+        foreach ($collection as $price) {
             $channelCode = $price->getChannel()->getCode();
 
-            if(
-                !isset($existingValues[$channelCode])
-            ){
+            if (!isset($existingValues[$channelCode])) {
                 $existingValues[$channelCode] = [];
             }
 
-            if(in_array($price->getCustomerOptionValue(), $existingValues[$channelCode])){
+            if (in_array($price->getCustomerOptionValue(), $existingValues[$channelCode])) {
                 $this->context->addViolation($constraint->message);
-            }else{
+            } else {
                 $existingValues[$channelCode][] = $price->getCustomerOptionValue();
             }
         }
