0eeea879a7a8a46ca2953ee7277620efd0e2cccc
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2018-02-28 16:07:21 +0100

    Added tests for customer option group fixture

--- tests/PHPUnit/Fixture/CustomerOptionGroupFixtureTest.php
+++ tests/PHPUnit/Fixture/CustomerOptionGroupFixtureTest.php
@@ -13,6 +13,107 @@ class CustomerOptionGroupFixtureTest extends TestCase
 {
     use ConfigurationTestCaseTrait;
 
+    /**
+     * @test
+     */
+    public function customer_option_groups_are_optional(){
+        $this->assertConfigurationIsValid([[]], 'customer_option_groups');
+    }
+
+    /**
+     * @test
+     */
+    public function can_be_randomly_generated(){
+        $this->assertConfigurationIsValid([['amount' => 4]], 'amount');
+    }
+
+    /**
+     * @test
+     */
+    public function code_is_required(){
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[
+                'code' => 'group'
+            ]]
+        ]], 'customer_option_groups.*.code');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'customer_option_groups' => [[
+
+            ]]
+        ]], 'customer_option_groups.*.code');
+    }
+
+    /**
+     * @test
+     */
+    public function needs_at_least_one_translation(){
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[
+                'translations' => [
+                    'en_US' => 'Group'
+                ]
+            ]]
+        ]], 'customer_option_groups.*.translations.*');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'customer_option_groups' => [[
+                'translations' => []
+            ]]
+        ]], 'customer_option_groups.*.translations.*');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'customer_option_groups' => [[]]
+        ]], 'customer_option_groups.*.translations');
+    }
+
+    /**
+     * @test
+     */
+    public function options_are_optional(){
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[
+                'options' => [
+                    'option_1',
+                    'option_2'
+                ]
+            ]]
+        ]], 'customer_option_groups.*.options');
+
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[
+                'options' => []
+            ]]
+        ]], 'customer_option_groups.*.options');
+
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[]]
+        ]], 'customer_option_groups.*.options');
+    }
+
+    /**
+     * @test
+     */
+    public function products_are_optional(){
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[
+                'products' => [
+                    'prod_1',
+                    'prod_2'
+                ]
+            ]]
+        ]], 'customer_option_groups.*.products');
+
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[
+                'products' => []
+            ]]
+        ]], 'customer_option_groups.*.products');
+
+        $this->assertConfigurationIsValid([[
+            'customer_option_groups' => [[]]
+        ]], 'customer_option_groups.*.products');
+    }
 
     /**
      * {@inheritdoc}
