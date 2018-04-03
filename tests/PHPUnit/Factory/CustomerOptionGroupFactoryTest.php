432d47693dc8db0f8d30c7986400c45dd2e30632
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2018-04-03 16:11:39 +0200

    Unit tests for conditional constraint fixtures

--- tests/PHPUnit/Factory/CustomerOptionGroupFactoryTest.php
+++ tests/PHPUnit/Factory/CustomerOptionGroupFactoryTest.php
@@ -6,6 +6,7 @@ namespace Tests\Brille24\SyliusCustomerOptionsPlugin\PHPUnit\Factory;
 
 use Brille24\SyliusCustomerOptionsPlugin\Entity\CustomerOptions\CustomerOption;
 use Brille24\SyliusCustomerOptionsPlugin\Entity\CustomerOptions\CustomerOptionGroup;
+use Brille24\SyliusCustomerOptionsPlugin\Entity\CustomerOptions\Validator\ValidatorInterface;
 use Brille24\SyliusCustomerOptionsPlugin\Entity\Product;
 use Brille24\SyliusCustomerOptionsPlugin\Factory\CustomerOptionGroupFactory;
 use Brille24\SyliusCustomerOptionsPlugin\Repository\CustomerOptionRepositoryInterface;
@@ -217,5 +218,116 @@ class CustomerOptionGroupFactoryTest extends TestCase
         $customerOptionGroup = $this->customerOptionGroupFactory->createFromConfig($options);
 
         $this->assertCount(3, $customerOptionGroup->getProducts());
+    }
+
+    /**
+     * @test
+     *
+     * @throws \Exception
+     */
+    public function testCreateWithConditionalConstraint()
+    {
+        $this->productRepositoryMock
+            ->expects($this->any())
+            ->method('findBy')
+            ->with(['code' => []])
+            ->willReturn([])
+        ;
+
+        $optionCodes = [
+            'option_1',
+            'option_2',
+            'option_3',
+        ];
+
+        $customerOptions = [];
+
+        foreach ($optionCodes as $index => $code) {
+            $option = new CustomerOption();
+            $option->setCode($code);
+
+            $this->customerOptionRepositoryMock
+                ->expects($this->at($index))
+                ->method('findOneByCode')
+                ->with($code)
+                ->willReturn($option)
+            ;
+
+            $customerOptions[] = $option;
+        }
+
+        $this->customerOptionRepositoryMock
+            ->expects($this->at(3))
+            ->method('findOneByCode')
+            ->with($customerOptions[0]->getCode())
+            ->willReturn($customerOptions[0]);
+
+        $this->customerOptionRepositoryMock
+            ->expects($this->at(4))
+            ->method('findOneByCode')
+            ->with($customerOptions[1]->getCode())
+            ->willReturn($customerOptions[1]);
+
+        $this->customerOptionRepositoryMock
+            ->expects($this->at(5))
+            ->method('findOneByCode')
+            ->with($customerOptions[1]->getCode())
+            ->willReturn($customerOptions[1]);
+
+        $this->customerOptionRepositoryMock
+            ->expects($this->at(6))
+            ->method('findOneByCode')
+            ->with($customerOptions[2]->getCode())
+            ->willReturn($customerOptions[2]);
+
+        $options = [
+            'code' => 'some_group',
+            'translations' => [
+                'en_US' => 'Some Group',
+            ],
+            'options' => $optionCodes,
+            'validators' => [
+                [
+                    'conditions' => [
+                        [
+                            'customer_option' => 'option_1',
+                            'comparator' => 'greater',
+                            'value' => '4',
+                        ],
+                    ],
+                    'constraints' => [
+                        [
+                            'customer_option' => 'option_2',
+                            'comparator' => 'lesser',
+                            'value' => '10',
+                        ],
+                        [
+                            'customer_option' => 'option_2',
+                            'comparator' => 'greater',
+                            'value' => '0',
+                        ],
+                        [
+                            'customer_option' => 'option_3',
+                            'comparator' => 'equal',
+                            'value' => '5',
+                        ],
+                    ],
+                    'error_messages' => [
+                        'en_US' => 'Test',
+                        'de_DE' => 'Test2',
+                    ],
+                ],
+            ],
+        ];
+
+        $customerOptionGroup = $this->customerOptionGroupFactory->createFromConfig($options);
+
+        $this->assertCount(1, $customerOptionGroup->getValidators());
+
+        /** @var ValidatorInterface $validator */
+        $validator = $customerOptionGroup->getValidators()[0];
+
+        $this->assertCount(1, $validator->getConditions());
+        $this->assertCount(3, $validator->getConstraints());
     }
 }
--- tests/PHPUnit/Fixture/CustomerOptionGroupFixtureTest.php
+++ tests/PHPUnit/Fixture/CustomerOptionGroupFixtureTest.php
@@ -122,6 +122,317 @@ class CustomerOptionGroupFixtureTest extends TestCase
     }
 
     /**
+     * @test
+     */
+    public function validators_are_optional()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[]],
+        ]], 'custom.*.validators');
+    }
+
+    /**
+     * @test
+     */
+    public function validators_need_at_least_one_element()
+    {
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [],
+            ]],
+        ]], 'custom.*.validators');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_conditions_are_optional()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[]],
+            ]],
+        ]], 'custom.*.validators.*.conditions');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_conditions_need_at_least_one_element()
+    {
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_constraints_are_optional()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[]],
+            ]],
+        ]], 'custom.*.validators.*.constraints');
+    }
+
+    public function validator_constraints_need_at_least_one_element()
+    {
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_condition_customer_option_is_required()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[
+                        'customer_option' => 'some_option',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.customer_option');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.customer_option');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[
+                        'customer_option' => '',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.customer_option');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_condition_comparator_is_required()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[
+                        'comparator' => 'greater',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.comparator');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.comparator');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[
+                        'comparator' => '',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.comparator');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_condition_value_is_required()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[
+                        'value' => '5',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.value');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.value');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'conditions' => [[
+                        'value' => '',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.conditions.*.value');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_constraint_customer_option_is_required()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[
+                        'customer_option' => 'some_option',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.customer_option');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.customer_option');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[
+                        'customer_option' => '',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.customer_option');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_constraint_comparator_is_required()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[
+                        'comparator' => 'greater',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.comparator');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.comparator');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[
+                        'comparator' => '',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.comparator');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_constraint_value_is_required()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[
+                        'value' => '5',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.value');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.value');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'constraints' => [[
+                        'value' => '',
+                    ]],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.constraints.*.value');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_error_messages_are_optional()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[]],
+            ]],
+        ]], 'custom.*.validators.*.error_messages');
+    }
+
+    /**
+     * @test
+     */
+    public function validator_error_messages_need_at_least_one_element()
+    {
+        $this->assertConfigurationIsValid([[
+            'custom' => [[
+                'validators' => [[
+                    'error_messages' => [
+                        'en_US' => 'Test',
+                    ],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.error_messages');
+
+        $this->assertPartialConfigurationIsInvalid([[
+            'custom' => [[
+                'validators' => [[
+                    'error_messages' => [],
+                ]],
+            ]],
+        ]], 'custom.*.validators.*.error_messages');
+    }
+
+    /**
      * {@inheritdoc}
      */
     protected function getConfiguration()
