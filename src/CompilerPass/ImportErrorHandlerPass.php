245f6b15ea23a3b7c6878a979758cee5ff8b14dd
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2020-04-07 08:19:16 +0200

    Price import - Fixed import error handler pass

--- src/CompilerPass/ImportErrorHandlerPass.php
+++ src/CompilerPass/ImportErrorHandlerPass.php
@@ -11,20 +11,25 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class ImportErrorHandlerPass implements CompilerPassInterface
 {
+    const SERVICE_ID = 'brille24.sylius_customer_options_plugin.handler.composite_import_error_handler';
+    const TAG_NAME   = 'brille24.sylius_customer_options_plugin.import_error_handler';
+
     public function process(ContainerBuilder $container): void
     {
-        if (!$container->hasDefinition('brille24.sylius_customer_options_plugin.handler.composite_import_error_handler')) {
+        if (!$container->hasDefinition(self::SERVICE_ID)) {
             return;
         }
 
-        $importErrorHandler = $container->getDefinition('brille24.sylius_customer_options_plugin.handler.composite_import_error_handler');
+        $importErrorHandler = $container->getDefinition(self::SERVICE_ID);
 
-        foreach ($container->findTaggedServiceIds('brille24.sylius_customer_options_plugin.import_error_handler') as $id => $attributes) {
+        foreach ($container->findTaggedServiceIds(self::TAG_NAME) as $id => $attributes) {
             if (!isset($attributes[0]['type'])) {
                 throw new \InvalidArgumentException('Tagged import error handlers need to have `type` attribute.');
             }
 
             $importErrorHandler->addMethodCall('addErrorHandler', [new Reference($id), $attributes[0]['type']]);
         }
+
+        $container->setDefinition(self::SERVICE_ID, $importErrorHandler);
     }
 }
