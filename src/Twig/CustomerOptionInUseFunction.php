2f3d02a30a3fc29cec3c8a89f83761e1b9b898f0
mamazu <maxo.pech@gmail.com>
2021-02-01 13:10:53 +0100

    Optimizing the customer option in use function

---
+++ src/Twig/CustomerOptionInUseFunction.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brille24\SyliusCustomerOptionsPlugin\Twig;
+
+use Brille24\SyliusCustomerOptionsPlugin\Entity\CustomerOptions\CustomerOptionInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class CustomerOptionInUseFunction extends AbstractExtension
+{
+    /** @var RepositoryInterface */
+    private $orderItemOptionRepository;
+
+    public function __construct(RepositoryInterface $orderItemOptionRepository)
+    {
+        $this->orderItemOptionRepository = $orderItemOptionRepository;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('customer_option_in_use', [$this, 'customerOptionIsInUse']),
+        ];
+    }
+
+    public function customerOptionIsInUse(CustomerOptionInterface $customerOption): bool
+    {
+        $orderItems = $this->orderItemOptionRepository
+            ->findBy(['customerOption' => $customerOption], null, 1);
+
+        return count($orderItems) > 0;
+    }
+}
