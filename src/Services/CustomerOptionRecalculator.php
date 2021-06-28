a53ee5593e63b43c05ba63a673752ea3d90b1055
Basil Baumgartner <basil.baumgartner@ongoing.ch>
2021-06-28 14:39:05 +0200

    refactor: optimized CustomerOptionRecalculator Events

--- src/Services/CustomerOptionRecalculator.php
+++ src/Services/CustomerOptionRecalculator.php
@@ -20,9 +20,9 @@ final class CustomerOptionRecalculator implements OrderProcessorInterface
     public const EVENT_POST_REMOVE_ADJUSTMENTS      = 'brille24.customer_option_recalculator_event.post.remove_adjustments';
 
     public const EVENT_PRE_ORDER_ITEM               = 'brille24.customer_option_recalculator_event.pre.order_item';
-    public const EVENT_CUSTOMER_OPTION              = 'brille24.customer_option_recalculator_event.customer_option';
-    public const EVENT_PREFIX_CUSTOMER_OPTION_TYPE  = 'brille24.customer_option_recalculator_event.customer_option.type.';
-    public const EVENT_PREFIX_CUSTOMER_OPTION_CODE  = 'brille24.customer_option_recalculator_event.customer_option.code.';
+    public const EVENT_ORDER_ITEM_OPTION            = 'brille24.customer_option_recalculator_event.order_item_option';
+    public const EVENT_PREFIX_ORDER_ITEM_OPTION_TYPE= 'brille24.customer_option_recalculator_event.order_item_option.type.';
+    public const EVENT_PREFIX_ORDER_ITEM_OPTION_CODE= 'brille24.customer_option_recalculator_event.order_item_option.code.';
     public const EVENT_POST_ORDER_ITEM              = 'brille24.customer_option_recalculator_event.post.order_item';
 
     public const CUSTOMER_OPTION_ADJUSTMENT = 'customer_option';
@@ -60,27 +60,27 @@ final class CustomerOptionRecalculator implements OrderProcessorInterface
                 continue;
             }
 
-            /** @var OrderItemOptionInterface[] $configuration */
-            $configuration = $orderItem->getCustomerOptionConfiguration();
-
             $this->eventDispatcher->dispatch(
                 self::EVENT_PRE_ORDER_ITEM,
                 new GenericEvent($orderItem)
             );
 
+            /** @var OrderItemOptionInterface[] $configuration */
+            $configuration = $orderItem->getCustomerOptionConfiguration();
+
             foreach ($configuration as $orderItemOption) {
 
                 $this->eventDispatcher->dispatch(
-                    self::EVENT_CUSTOMER_OPTION,
-                    new GenericEvent($orderItem)
+                    self::EVENT_ORDER_ITEM_OPTION,
+                    new GenericEvent($orderItemOption)
                 );
                 $this->eventDispatcher->dispatch(
-                    self::EVENT_PREFIX_CUSTOMER_OPTION_TYPE.$orderItemOption->getCustomerOptionType(),
-                    new GenericEvent($orderItem)
+                    self::EVENT_PREFIX_ORDER_ITEM_OPTION_TYPE.$orderItemOption->getCustomerOptionType(),
+                    new GenericEvent($orderItemOption)
                 );
                 $this->eventDispatcher->dispatch(
-                    self::EVENT_PREFIX_CUSTOMER_OPTION_CODE.$orderItemOption->getCustomerOptionCode(),
-                    new GenericEvent($orderItem)
+                    self::EVENT_PREFIX_ORDER_ITEM_OPTION_CODE.$orderItemOption->getCustomerOptionCode(),
+                    new GenericEvent($orderItemOption)
                 );
 
                 // Skip all customer options that don't have customer option values as they can not have a price like
