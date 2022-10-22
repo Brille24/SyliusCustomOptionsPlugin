a5e175bf04d1055431f068ef70e3a4e4232518e5
Francis Hilaire <Prometee@users.noreply.github.com>
2022-10-22 02:28:58 +0200

    Remove unnecessary requirements and allow more Sylius versions (#117)
    * Remove unnecessary requirements and allow more Sylius versions
    
    * [Build Sylius 1.12] Update composer.json to execute needed symlink script
    
    * [Build Sylius 1.12] Switch to webpack on test app
    
    * [Build Sylius 1.12] Add Sylius 1.12 dev to the build
    
    * Update webpack config
    
    * Fix Sylius 1.12 error with missing SwiftMailer
    
    * Add require new mailer, security and framework config
    
    * Add missing webpack changes
    
    * Avoid deprecated warning during BeHat tests
    
    * Fix line return
    
    * Fix webpack entry points

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -4,7 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sylius\Bundle\OrderBundle\SyliusOrderBundle::class => ['all' => true],
@@ -54,4 +53,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
--- tests/Application/config/sylius/1.9/bundles.php
+++ tests/Application/config/sylius/1.9/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
